6800917e30a06d03af7c60648c4b324fa9bf09b2
javad.memariani <javad.memariani@gmail.com>
2023-07-05 15:09:20 +0330

    setup solution

---
+++ source/Crud.Api/Program.cs
@@ -0,0 +1,18 @@
+namespace Crud.Api;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        CreateHostBuilder(args).Build().Run();
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args)
+    {
+        return Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
---
+++ source/Crud.Api/Startup.cs
@@ -0,0 +1,39 @@
+﻿namespace Crud.Api;
+
+public class Startup
+{
+    public IConfiguration Configuration { get; }
+
+    private IWebHostEnvironment CurrentEnvironment { get; set; }
+
+    public Startup(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
+    {
+        Configuration = configuration;
+        CurrentEnvironment = webHostEnvironment;
+    }
+
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddOptions();
+
+        services.AddHttpContextAccessor();
+
+        services.AddControllers();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseRouting();
+
+        app.UseAuthorization();
+
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapControllers();
+        });
+    }
+}
+
+
+
