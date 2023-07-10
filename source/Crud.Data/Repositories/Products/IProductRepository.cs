a0d72ae22a482a5541280a5f8835a8ab4107f412
javad.memariani <javad.memariani@gmail.com>
2023-07-10 15:15:18 +0330

    rename product repository

--- source/Crud.Data/Repositories/Products/IProductRepository.cs
+++ source/Crud.Data/Repositories/Products/IProductRepository.cs
@@ -3,7 +3,14 @@ using Crud.Domain.Entities;
 
 namespace Crud.Data.Repositories.Products;
 
+/// <summary>
+/// Represents a repository interface for product entities.
+/// </summary>
 public interface IProductRepository : IRepository<Product>
 {
+    /// <summary>
+    /// Retrieves a list of all products.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the list of products.</returns>
     Task<List<Product>> GetProducts();
 }
--- source/Crud.Data/Repositories/Products/ProductRepository.cs
+++ source/Crud.Data/Repositories/Products/ProductRepository.cs
@@ -5,10 +5,17 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Crud.Data.Repositories.Products;
 
+/// <summary>
+/// Represents a repository implementation for product entities.
+/// </summary>
 public class ProductRepository : Repository<Product>, IProductRepository
 {
     public ProductRepository(CrudDbContext context) : base(context) { }
 
+    /// <summary>
+    /// Retrieves a list of all products.
+    /// </summary>
+    /// <returns>A task that represents the asynchronous operation. The task result contains the list of products.</returns>
     public async Task<List<Product>> GetProducts()
     {
         return await CrudDbContext
