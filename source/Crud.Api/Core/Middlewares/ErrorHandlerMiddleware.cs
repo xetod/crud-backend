84e6a892c10fbb2a46196e363fdc058d783ea709
javad.memariani <javad.memariani@gmail.com>
2023-07-14 14:50:17 +0330

    add error handler middleware

---
+++ source/Crud.Api/Core/Middlewares/ErrorHandlerMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Text.Json;
+using Crud.Application.Core.Result;
+
+namespace Crud.Api.Core.Middlewares;
+
+public class ErrorHandlerMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger _logger;
+
+    public ErrorHandlerMiddleware(RequestDelegate next, ILogger<ErrorHandlerMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception error)
+        {
+            var response = context.Response;
+
+            response.ContentType = "application/json";
+
+            response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var result = JsonSerializer.Serialize(Result.Fail(error.Message));
+
+            _logger.LogCritical(error, error.Message);
+
+            await response.WriteAsync(result);
+        }
+    }
+}
